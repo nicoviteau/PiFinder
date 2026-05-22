e486b7a1a9976cea38740b8d4e485310bad50703
Nicolas V. <72795699+nicoviteau@users.noreply.github.com>
2026-05-22 12:36:04 +0200

    Update config_display.py

--- python/PiFinder/ui/config_display.py
+++ python/PiFinder/ui/config_display.py
@@ -2,9 +2,10 @@
 # Configuration file to choose local display, remote display or both
 
 # Constants representing the available display modes
-DISPLAY_MODE_LOCAL = 0
-DISPLAY_MODE_REMOTE = 1
-DISPLAY_MODE_MIRROR = 2   # Display is duplicated both locally and remotely (mirror mode)
+
+DISPLAY_MODE_LOCAL = "local"
+DISPLAY_MODE_REMOTE = "remote"
+DISPLAY_MODE_MIRROR = "mirror"  # Display is duplicated both locally and remotely (mirror mode)
 
 # Active display mode
 # LOCAL is chosen as the default (compaibility with "standard" PiFinder)
